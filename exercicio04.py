86fc645c377650c90c7ce320fb46e8c5e2e29697
Thomas Laureano <thomaslaureano11@gmail.com>
2024-08-12 20:37:59 -0300

    Criação do Exercício 04
    Adição do Exercício 04 ao repositório Exercicios-Python

---
+++ exercicio04.py
@@ -0,0 +1,7 @@
+num1 = int(input("Informe o primeiro número: "))
+num2 = int(input("Informe o segundo número: "))
+
+if (num1 > num2):
+    print(f"O maior número é {num1}")  
+elif (num1 < num2):
+    print(f"O maior número é {num2}")
