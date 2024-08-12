1d642fcabdc4dfa5adec4df33ff6742fa4904bce
Thomas Laureano <thomaslaureano11@gmail.com>
2024-08-12 20:27:28 -0300

    Criação do Exercício 03
    Adição do Exercício 03 ao repositório Exercicios-Python

---
+++ exercicio03.py
@@ -0,0 +1,5 @@
+numero1 = int(input("Informe o primeiro número: "))
+numero2 = int(input("Informe o segundo número: "))
+numero3 = int(input("Informe o terceiro número: "))
+resultado = numero1+numero2+numero3
+print(f"A soma dos números é {resultado}")
