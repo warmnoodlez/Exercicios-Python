b26f5617d43294c633b73b978186d1d009acc219
Thomas Laureano <thomaslaureano11@gmail.com>
2024-08-28 21:08:50 -0300

    Correção e conclusão do Exercício 55

--- exercicio55.py
+++ exercicio55.py
@@ -6,16 +6,16 @@ def substituir_acentos(string):
         string = string.replace(acento, ' ')
     return string
 
-frase_alt = frase.lower()
-frase_alt = substituir_acentos(frase_alt)
-frase_alt = frase_alt.strip(' ')
-tmn_frase = len(frase_alt)
-print(frase_alt)
-frase_inv = frase_alt[::-1]
+frase_inv = frase.lower()
+frase_inv = substituir_acentos(frase_inv)
+frase_inv = frase_inv.replace(" ", "")
+tmn_frase = len(frase_inv)
+print(frase_inv)
+frase_inv = frase_inv[::-1]
 print(frase_inv)
 
 for i in range (0,tmn_frase):
-    if frase_alt[i] == frase_inv[i]:
+    if frase_inv[i] == frase_inv[i]:
         condicao = True
     else:
         condicao = False
