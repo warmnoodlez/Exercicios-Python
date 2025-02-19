017a5fe17a79e0f6e67c347ac065b32f48c8a06f
Thomas Laureano <thomaslaureano11@gmail.com>
2025-02-19 18:40:28 -0300

    Modificação do Exercício 1
    Modifiquei a estrutura do exercício 1 para poder executar as funções do novo módulo criado.

--- Exercicios-2025/exercicio1.py
+++ Exercicios-2025/exercicio1.py
@@ -1,17 +1,11 @@
 # Exercício 1 - Validador de CPF
 
 # Criando função para resetar o programa.
-import sys
-import os
-
-def resetar_programa():
-    python = sys.executable
-    os.execl(python, python, * sys.argv)
+import funcoes
 
 # Definindo Listas
-resultado = []
-digitos_cpf = []
-verificadores = []
+listas = []
+calc_verificadores = []
 
 # Informando o CPF:
 cpf = input("\nInforme o seu CPF: ")
@@ -20,67 +14,18 @@ cpf = input("\nInforme o seu CPF: ")
 condicao = cpf.isdigit()
 
 # Testando condição:
-if condicao == True:
-    organizando_digitos()
-    primeiro_digito()
-    segundo_digito()
-    teste_final()
+while condicao != True:
+    print("Caracteres inválidos detectados, tente novamente!")
+    cpf = input("\nInforme o seu CPF: ")
+    condicao = cpf.isdigit()
 else:
-    resetar_programa()
+    # Organizando digitos do CPF em listas: 
+    listas = funcoes.organizando_digitos(cpf)
+    num_inscricao = listas[0]
+    verificadores_cpf = listas[1]
+    digitos_cpf = listas[2]
 
-# Definindo funções:
-
-# Organizando os digitos em listas.
-def organizando_digitos():
-    # Separando digitos individualmente em uma lista e os convertendo de Caracter para Inteiros
-    digitos_cpf_strg = list(cpf)
-    for val in digitos_cpf_strg:
-        digitos_cpf.append(int(val))
-
-    # Separando os digitos do CPF em duas listas: inscrição e os digitos verificadores.
-    num_inscricao = digitos_cpf[:9]
-    verificadores_cpf = digitos_cpf[-2:]
-
-
-# Fazendo o cálculo para obter o primeiro digito.
-def primeiro_digito():
-    b = -1
-    for a in range(10,1,-1):
-        b = b + 1
-        resultado.append(a*digitos_cpf[b])
-
-    soma = sum(resultado)
-    resto = soma % 11
-
-    if resto < 2:
-        verificadores.append(0)
-    else:
-        verificadores.append(11-resto)
-
-
-
-
-# Fazendo o cálculo para obter o segundo digito.
-def segundo_digito():
-    # Resetando elementos para o novo cálculo.
-    resultado.clear()
-    b = -1 
-
-    for a in range(11,1,-1):
-        b = b + 1
-        resultado.append(a*digitos_cpf[b])
-
-    soma = sum(resultado)
-    resto = soma % 11
-
-    if resto < 2:
-        verificadores.append(0)
-    else:
-        verificadores.append(11-resto)
-
-# Testando se o CPF é válido ou não.
-def teste_final():
-    if verificadores == verificadores_cpf:
-        print("O CPF informado é valido! :)")
-    else:
-        print("O CPF informado não é válido. :(")
+    calc_verificadores.append(funcoes.primeiro_digito(digitos_cpf))
+    calc_verificadores.append(funcoes.segundo_digito(calc_verificadores, digitos_cpf))
+    
+    funcoes.teste_final(calc_verificadores, verificadores_cpf)
